25aeb304c4b93bfc225a497e84946401fd1b8567
Sean Oughton <seanoughton@gmail.com>
2018-03-15 17:43:37 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,7 @@ require 'active_support/inflector'
 
 class Song
 
-
+  #this takes the class name, turns it into a lowercase string and pluralizes it
   def self.table_name
     self.to_s.downcase.pluralize
   end
@@ -31,11 +31,6 @@ class Song
     end
   end
 
-  def save
-    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
-    DB[:conn].execute(sql)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
-  end
 
   def table_name_for_insert
     self.class.table_name
@@ -53,12 +48,15 @@ class Song
     self.class.column_names.delete_if {|col| col == "id"}.join(", ")
   end
 
+  def save
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+    DB[:conn].execute(sql)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+  end
+
   def self.find_by_name(name)
     sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
     DB[:conn].execute(sql)
   end
 
 end
-
-
-
